c5e28cb6fa37ab78f6f9427ec945e4320e92d8d5
RaghavSobti37 <raghavraj.sobti.21cse@bmu.edu.in>
2025-04-02 17:45:09 +0530

    Working connection of testdb with dummy input

--- server/index.js
+++ server/index.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const connectDB = require('./db.js')
-const itemModels = require('./models/item.js')
+const cors = require('cors');
+const itemModel = require('./models/item.js');
 
 const app = express();
+app.use(express.json());
+app.use(cors());
+connectDB();
 
-connectDB()
+app.get('/' , async (req,res) => {
+    const response = await itemModel.find()
+    return res.json({items : response})
+})
 
 app.listen(3000, ()=> {
   console.log("App is Running")
