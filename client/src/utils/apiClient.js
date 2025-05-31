bed472a5ffd9d03603bdcd1acbc6e122a883f922
RaghavSobti37 <raghavraj.sobti.21cse@bmu.edu.in>
2025-06-01 00:08:21 +0530

    update server build

--- client/src/utils/apiClient.js
+++ client/src/utils/apiClient.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:3000/api';
+const API_BASE_URL = 'https://ekors-erp.onrender.com/api';
 
 const apiClient = async (endpoint, { body, method = 'GET', ...customConfig } = {}) => {
   const token = localStorage.getItem('erp-user');
